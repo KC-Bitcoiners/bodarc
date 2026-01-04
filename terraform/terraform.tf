0f16c6f8b3498e84ef877ccdcb5b60486da87999
auggie-lahey <15633227+auggie-lahey@users.noreply.github.com>
2026-01-04 16:23:03 -0600

    A records?

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -3,7 +3,6 @@ resource "dreamhost_dns_record" "dns_records" {
   for_each = var.dns_records
 
   record = "${each.key}.kcbitcoiners.com"
-  type   = each.type
-  value  = each.points_to
+  type   = each.value.type
+  value  = each.value.points_to
 }
-
